68d54ac943ba8f769918df5453c91d3403365763
protonbraulio <nortechnologies@proton.me>
2026-05-19 14:55:11 +0800

    chore: seed site template

---
+++ astro.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import { SITE } from "./src/config";
+
+export default defineConfig({
+  site: SITE.website,
+  integrations: [
+    mdx({
+      extendMarkdownConfig: true,
+    }),
+    sitemap({
+      filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    shikiConfig: {
+      themes: { light: "min-light", dark: "github-dark-default" },
+      defaultColor: false,
+      wrap: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+  },
+});
---
+++ src/constants.ts
@@ -0,0 +1,58 @@
+import type { Props } from "astro";
+import IconMail from "@/assets/icons/IconMail.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconBrandX from "@/assets/icons/IconBrandX.svg";
+import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+import IconFacebook from "@/assets/icons/IconFacebook.svg";
+import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import { SITE } from "@/config";
+
+interface Social {
+  name: string;
+  href: string;
+  linkTitle: string;
+  icon: (_props: Props) => Element;
+}
+
+export const SOCIALS: Social[] = [] as const;
+
+export const SHARE_LINKS: Social[] = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/?text=",
+    linkTitle: `Send este post por WhatsApp`,
+    icon: IconWhatsapp,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/sharer.php?u=",
+    linkTitle: `Share este post en Facebook`,
+    icon: IconFacebook,
+  },
+  {
+    name: "X",
+    href: "https://x.com/intent/post?url=",
+    linkTitle: `Share este post en X`,
+    icon: IconBrandX,
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/share/url?url=",
+    linkTitle: `Share este post por Telegram`,
+    icon: IconTelegram,
+  },
+  {
+    name: "Pinterest",
+    href: "https://pinterest.com/pin/create/button/?url=",
+    linkTitle: `Share este post en Pinterest`,
+    icon: IconPinterest,
+  },
+  {
+    name: "Mail",
+    href: "mailto:?subject=See%20this%20post&body=",
+    linkTitle: `Send este post por correo`,
+    icon: IconMail,
+  },
+] as const;
